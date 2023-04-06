be0f314b1c97cee0d3b54d93728cc7d216c01ad9
Eze Williams <ezewilliamsezebuilo1010@gmail.com>
2023-04-06 18:18:54 +0100

    feat: this commit implemment the styles for the sliding carousel

--- src/Test.jsx
+++ src/Test.jsx
@@ -4,14 +4,28 @@ import "./App.css";
 export default function Test() {
   return (
     <div className="div">
+      <button className="prev">prev</button>
       <div className="carouselContainer">
-        <div className="carousel"></div>
-        <div className="carousel"></div>
-        <div className="carousel"></div>
-        <div className="carousel"></div>
-        <div className="carousel"></div>
-        <div className="carousel"></div>
+        <div className="carousel">
+          <div className="list"></div>
+        </div>
+        <div className="carousel">
+          <div className="list"></div>
+        </div>
+        <div className="carousel">
+          <div className="list"></div>
+        </div>
+        <div className="carousel">
+          <div className="list"></div>
+        </div>
+        <div className="carousel">
+          <div className="list"></div>
+        </div>
+        <div className="carousel">
+          <div className="list"></div>
+        </div>
       </div>
+      <button className="next">next</button>
     </div>
   );
 }
