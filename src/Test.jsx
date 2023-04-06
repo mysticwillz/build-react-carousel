a1038121e05b0eed3b6706207102052f41895f60
Eze Williams <ezewilliamsezebuilo1010@gmail.com>
2023-04-06 17:59:05 +0100

    feat: this commit structures the carousel styles

---
+++ src/Test.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+export default function Test() {
+  return (
+    <div className="div">
+      <div className="carouselContainer">
+        <div className="carousel"></div>
+        <div className="carousel"></div>
+        <div className="carousel"></div>
+        <div className="carousel"></div>
+        <div className="carousel"></div>
+        <div className="carousel"></div>
+      </div>
+    </div>
+  );
+}
